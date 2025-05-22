5281f2ba64092097e7dd1acb87857d1a00208393
Adam.Wang <bddiudiu@gmail.com>
2025-05-22 13:58:05 +0800

    feat: 火山引擎增加文生图

--- relay/relay-image.go
+++ relay/relay-image.go
@@ -18,6 +18,7 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"one-api/relay/constant"
 )
 
 func getAndValidImageRequest(c *gin.Context, info *relaycommon.RelayInfo) (*dto.ImageRequest, error) {
@@ -40,6 +41,11 @@ func getAndValidImageRequest(c *gin.Context, info *relaycommon.RelayInfo) (*dto.
 			if imageRequest.Quality == "" {
 				imageRequest.Quality = "standard"
 			}
+		}
+
+		if info.ApiType == constant.APITypeVolcEngine {
+			watermark := formData.Has("watermark")
+			imageRequest.Watermark = &watermark
 		}
 	default:
 		err := common.UnmarshalBodyReusable(c, imageRequest)
