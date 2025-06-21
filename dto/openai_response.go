ef32cc8e0a3cee33aecdd9eb4bf59f9b6f4f4097
CaIon <1808837298@qq.com>
2025-06-22 00:59:39 +0800

    ✨ fix(dto): change Created field type in OpenAITextResponse to any. (close #1131)

--- dto/openai_response.go
+++ dto/openai_response.go
@@ -26,7 +26,7 @@ type OpenAITextResponse struct {
 	Id      string                     `json:"id"`
 	Model   string                     `json:"model"`
 	Object  string                     `json:"object"`
-	Created int64                      `json:"created"`
+	Created any                        `json:"created"`
 	Choices []OpenAITextResponseChoice `json:"choices"`
 	Error   *OpenAIError               `json:"error,omitempty"`
 	Usage   `json:"usage"`
