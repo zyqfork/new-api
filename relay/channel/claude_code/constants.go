fe9acb6c5960859acf13ca84a48789b7aa22cb16
Seefs <i@seefs.me>
2025-07-26 18:40:18 +0800

    chore: claude code automatic disable

--- relay/channel/claude_code/constants.go
+++ relay/channel/claude_code/constants.go
@@ -2,7 +2,6 @@ package claude_code
 
 var ModelList = []string{
 	"claude-3-5-haiku-20241022",
-	"claude-3-5-sonnet-20240620",
 	"claude-3-5-sonnet-20241022",
 	"claude-3-7-sonnet-20250219",
 	"claude-3-7-sonnet-20250219-thinking",
