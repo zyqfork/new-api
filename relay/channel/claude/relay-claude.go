a8462c1b70c73769ee452989487693039f1da3a8
IcedTangerine <baffling4945@outlook.com>
2025-07-30 12:17:56 +0800

    Revert "Update relay-claude.go"

--- relay/channel/claude/relay-claude.go
+++ relay/channel/claude/relay-claude.go
@@ -185,10 +185,7 @@ func RequestOpenAI2ClaudeMessage(textRequest dto.GeneralOpenAIRequest) (*dto.Cla
 		}
 		// TODO: 临时处理
 		// https://docs.anthropic.com/en/docs/build-with-claude/extended-thinking#important-considerations-when-using-extended-thinking
-		// Anthropic 要求去掉 top_k
-		claudeRequest.TopK = nil
-		//top_p值可以在0.95-1之间
-		claudeRequest.TopP = 0.95
+		claudeRequest.TopP = 0
 		claudeRequest.Temperature = common.GetPointer[float64](1.0)
 		claudeRequest.Model = strings.TrimSuffix(textRequest.Model, "-thinking")
 	}
