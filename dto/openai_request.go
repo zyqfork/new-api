50f9195f2d26882292c18a168a63e046cc5fda10
CaIon <i@caion.me>
2025-08-09 21:04:49 +0800

    feat: remove custom JSON marshaling for Message struct

--- dto/openai_request.go
+++ dto/openai_request.go
@@ -140,14 +140,6 @@ type Message struct {
 	//parsedStringContent *string
 }
 
-func (m *Message) MarshalJSON() ([]byte, error) {
-	if m.Content == nil {
-		m.Content = ""
-	}
-	type Alias Message
-	return json.Marshal((*Alias)(m))
-}
-
 type MediaContent struct {
 	Type       string `json:"type"`
 	Text       string `json:"text,omitempty"`
