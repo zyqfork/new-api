abcb35379389ba15d25d3d13512faea6aca227e7
Nekohy <qaq3254865169@gmail.com>
2025-08-24 01:02:23 +0800

    feats:add custom headers override

--- model/channel.go
+++ model/channel.go
@@ -46,6 +46,7 @@ type Channel struct {
 	Tag               *string `json:"tag" gorm:"index"`
 	Setting           *string `json:"setting" gorm:"type:text"` // 渠道额外设置
 	ParamOverride     *string `json:"param_override" gorm:"type:text"`
+	HeaderOverride    *string `json:"header_override" gorm:"type:text"`
 	// add after v0.8.5
 	ChannelInfo ChannelInfo `json:"channel_info" gorm:"type:json"`
 
@@ -873,6 +874,17 @@ func (channel *Channel) GetParamOverride() map[string]interface{} {
 		}
 	}
 	return paramOverride
+}
+
+func (channel *Channel) GetHeaderOverride() map[string]interface{} {
+	headerOverride := make(map[string]interface{})
+	if channel.HeaderOverride != nil && *channel.HeaderOverride != "" {
+		err := common.Unmarshal([]byte(*channel.HeaderOverride), &headerOverride)
+		if err != nil {
+			common.SysLog(fmt.Sprintf("failed to unmarshal param override: channel_id=%d, error=%v", channel.Id, err))
+		}
+	}
+	return headerOverride
 }
 
 func GetChannelsByIds(ids []int) ([]*Channel, error) {
