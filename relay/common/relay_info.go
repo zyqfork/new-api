81184240390b21aa37c04d3426755e65c70e266e
Seefs <i@seefs.me>
2025-09-30 09:46:46 +0800

    fix: claude beta=true

--- relay/common/relay_info.go
+++ relay/common/relay_info.go
@@ -105,7 +105,8 @@ type RelayInfo struct {
 	UserQuota              int
 	RelayFormat            types.RelayFormat
 	SendResponseCount      int
-	FinalPreConsumedQuota  int // 最终预消耗的配额
+	FinalPreConsumedQuota  int  // 最终预消耗的配额
+	IsClaudeBetaQuery      bool // /v1/messages?beta=true
 
 	PriceData types.PriceData
 
@@ -278,6 +279,9 @@ func GenRelayInfoClaude(c *gin.Context, request dto.Request) *RelayInfo {
 	info.ShouldIncludeUsage = false
 	info.ClaudeConvertInfo = &ClaudeConvertInfo{
 		LastMessagesType: LastMessageTypeNone,
+	}
+	if c.Query("beta") == "true" {
+		info.IsClaudeBetaQuery = true
 	}
 	return info
 }
