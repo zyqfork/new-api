3172c956f7fc0199e26a0ed1851fbf7e081def97
etnAtker <adsonlies@hotmail.com>
2025-10-13 20:06:33 +0800

    feat: 添加SiliconFlow图像生成接口自动转换支持
    1. 将对SiliconFlow渠道的RelayModeImagesGenerations请求，转发至v1/images/generations端点。
    2. SiliconFlow图像生成接口额外参数适配。

--- relay/channel/siliconflow/dto.go
+++ relay/channel/siliconflow/dto.go
@@ -15,3 +15,18 @@ type SFRerankResponse struct {
 	Results []dto.RerankResponseResult `json:"results"`
 	Meta    SFMeta                     `json:"meta"`
 }
+
+type SFImageRequest struct {
+	Model             string  `json:"model"`
+	Prompt            string  `json:"prompt"`
+	NegativePrompt    string  `json:"negative_prompt,omitempty"`
+	ImageSize         string  `json:"image_size,omitempty"`
+	BatchSize         uint    `json:"batch_size,omitempty"`
+	Seed              uint64  `json:"seed,omitempty"`
+	NumInferenceSteps uint    `json:"num_inference_steps,omitempty"`
+	GuidanceScale     float64 `json:"guidance_scale,omitempty"`
+	Cfg               float64 `json:"cfg,omitempty"`
+	Image             string  `json:"image,omitempty"`
+	Image2            string  `json:"image2,omitempty"`
+	Image3            string  `json:"image3,omitempty"`
+}
