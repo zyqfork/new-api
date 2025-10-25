6791eb72ba163043babb911a1a64447bb5b16775
CaIon <i@caion.me>
2025-10-25 22:10:21 +0800

    feat: add support for Submodel channel type in relay info

--- relay/common/relay_info.go
+++ relay/common/relay_info.go
@@ -264,6 +264,7 @@ var streamSupportedChannels = map[int]bool{
 	constant.ChannelTypeBaiduV2:    true,
 	constant.ChannelTypeZhipu_v4:   true,
 	constant.ChannelTypeAli:        true,
+	constant.ChannelTypeSubmodel:   true,
 }
 
 func GenRelayInfoWs(c *gin.Context, ws *websocket.Conn) *RelayInfo {
