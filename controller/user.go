9aeef6abecc209d076cf357cd4dc6200524ed861
skynono <feitianbubu@qq.com>
2025-12-26 13:59:56 +0800

    feat: support first bind update password (#2520)

--- controller/user.go
+++ controller/user.go
@@ -763,7 +763,10 @@ func checkUpdatePassword(originalPassword string, newPassword string, userId int
 	if err != nil {
 		return
 	}
-	if !common.ValidatePasswordAndHash(originalPassword, currentUser.Password) {
+
+	// 密码不为空,需要验证原密码
+	// 支持第一次账号绑定时原密码为空的情况
+	if !common.ValidatePasswordAndHash(originalPassword, currentUser.Password) && currentUser.Password != "" {
 		err = fmt.Errorf("原密码错误")
 		return
 	}
