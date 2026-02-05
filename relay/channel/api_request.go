b55e42eda7409d0749d82d24e8c942d9fb0b4776
CaIon <i@caion.me>
2026-02-05 22:16:35 +0800

    feat(api): add 'cookie' to passthroughSkipHeaderNamesLower

--- relay/channel/api_request.go
+++ relay/channel/api_request.go
@@ -58,6 +58,8 @@ var passthroughSkipHeaderNamesLower = map[string]struct{}{
 	"transfer-encoding":   {},
 	"upgrade":             {},
 
+	"cookie": {},
+
 	// Additional headers that should not be forwarded by name-matching passthrough rules.
 	"host":           {},
 	"content-length": {},
