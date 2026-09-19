58a9eff4c8421a6bbb04855e561310d3eeabd107
CaIon <i@caion.me>
2026-09-19 16:50:35 +0800

    style: refresh logo

--- web/rsbuild.config.ts
+++ web/rsbuild.config.ts
@@ -64,6 +64,7 @@ export default defineConfig(({ envMode }) => {
     },
     html: {
       template: './index.html',
+      favicon: './public/favicon.ico',
     },
     server: {
       host: '0.0.0.0',
